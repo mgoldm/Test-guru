ec4fcfe4dad99a4faffdb66a779e5d6efa2b72f0
mgoldm <57058926+mgoldm@users.noreply.github.com>
2021-12-13 15:50:02 +0300

    Update 20211212094513_create_tests.rb

--- db/migrate/20211212094513_create_tests.rb
+++ db/migrate/20211212094513_create_tests.rb
@@ -4,6 +4,7 @@ class CreateTests < ActiveRecord::Migration[6.1]
       t.string :title
       t.integer :level
       t.references :category, null: false, foreign_key: true
+      t.references :users, null:false, foreign_key:true
       t.timestamps
     end
   end
