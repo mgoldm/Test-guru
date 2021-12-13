a6d464f91b629770f6603af55d65ff06fc1965e3
mgoldm <57058926+mgoldm@users.noreply.github.com>
2021-12-13 18:23:14 +0300

    Update 20211212094513_create_tests.rb

--- db/migrate/20211212094513_create_tests.rb
+++ db/migrate/20211212094513_create_tests.rb
@@ -1,10 +1,10 @@
 class CreateTests < ActiveRecord::Migration[6.1]
   def change
     create_table :tests do |t|
-      t.string :title
-      t.integer :level
+      t.string :title, null: false
+      t.integer :level, null: false
       t.references :category, null: false, foreign_key: true
-      t.references :users, null:false, foreign_key:true
+      t.references :author, null: false, foreign_key: { to_table: :users }
       t.timestamps
     end
   end
