5d781b9c3244a63f21735405515d036695301237
mgoldm <57058926+mgoldm@users.noreply.github.com>
2021-12-14 11:41:37 +0300

    Update 20211212095906_create_answers.rb

--- db/migrate/20211212095906_create_answers.rb
+++ db/migrate/20211212095906_create_answers.rb
@@ -1,7 +1,7 @@
 class CreateAnswers < ActiveRecord::Migration[6.1]
   def change
     create_table :answers do |t|
-      t.string :title
+      t.string :title, null: false
       t.boolean :correct, default: false, null: false
       t.references :question, null: false, foreign_key: true
       t.timestamps
