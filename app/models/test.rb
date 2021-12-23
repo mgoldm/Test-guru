a4bdd03a1d74c5bf7e2d123a227f4922c965df1f
Nick Smirnov <smirnovnickolay00@gmail.com>
2021-12-23 11:00:36 +0300

    Задание 3 Добавил сущность result

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,5 +1,10 @@
 class Test < ApplicationRecord
+  has_many :questions
+  has_many :users, through: :results
+  belongs_to :category
+  belongs_to :author, class_name: 'User', foreign_key: 'author_id'
+
   def self.sort(category)
-    Test.joins('JOIN categories on tests.category_id=categories.id').where(categories: {title: category }).order(:title, :desc).pluck(:title)
+    Test.joins('JOIN categories on tests.category_id=categories.id').where(categories: { title: category }).order(:title, :desc).pluck(:title)
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ApplicationRecord
+  has_many :tests, through: :results
+
   def users(level)
-    Test.joins('JOIN results on tests.id=results.test_id').where(results: {user_id: id}, tests: {level: level})
+    Test.joins('JOIN results on tests.id=results.test_id').where(results: { user_id: id }, tests: { level: level })
   end
 end
