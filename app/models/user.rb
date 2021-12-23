4db159545a7dfd8590fb352a20de64f4d520b0ed
Nick Smirnov <smirnovnickolay00@gmail.com>
2021-12-23 17:19:43 +0300

    добавил ассоциацию create_test

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  has_many :create_tests, class_name: 'Test', foreign_key: 'author_id', dependent: :destroy
   has_many :tests, through: :results
   has_many :results, dependent: :destroy
 
