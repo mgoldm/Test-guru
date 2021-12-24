bec4cb361c4abad3a645d0418cf7499c35a7c332
Nick Smirnov <smirnovnickolay00@gmail.com>
2021-12-24 12:43:54 +0300

    Исправил функцию user_tests

--- app/models/result.rb
+++ app/models/result.rb
@@ -1,7 +1,4 @@
 class Result < ApplicationRecord
   belongs_to :test
   belongs_to :user
-
-  has_many :users, dependent: :destroy
-  has_many :tests, dependent: :destroy
 end
--- app/models/test.rb
+++ app/models/test.rb
@@ -1,9 +1,9 @@
 class Test < ApplicationRecord
   belongs_to :category
   belongs_to :author, class_name: 'User', foreign_key: 'author_id'
-  belongs_to :result
 
   has_many :questions, dependent: :destroy
+  has_many :results, dependent: :destroy
   has_many :users, through: :results
 
   def self.all_category_tests(category_title)
