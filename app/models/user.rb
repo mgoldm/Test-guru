ec4a3952ea0c16ef33137b29894dd2a14c0c5b8f
Nick Smirnov <smirnovnickolay00@gmail.com>
2021-12-26 09:52:05 +0300

    module 5 scope and validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,9 +5,7 @@ class User < ApplicationRecord
   has_many :tests, through: :results
   has_many :tests, through: :results
 
-
   validates :name, presence: true
   validates :email, presence: true
-
 
 end
