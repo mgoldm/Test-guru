ee814dec078449420ac86afabeeb879ad10de7e4
Nick Smirnov <smirnovnickolay00@gmail.com>
2021-12-26 09:51:57 +0300

    module 5 scope and validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,13 @@
 class User < ApplicationRecord
   has_many :create_tests, class_name: 'Test', foreign_key: 'author_id', dependent: :destroy
   has_many :results, dependent: :destroy
+  has_many :results, dependent: :destroy
+  has_many :tests, through: :results
+  has_many :tests, through: :results
+
 
   validates :name, presence: true
   validates :email, presence: true
 
-  has_many :results, dependent: :destroy
-  has_many :tests, through: :results
-  has_many :tests, through: :results
 
 end
